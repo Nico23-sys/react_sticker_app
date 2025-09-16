0e0695c012f18e1f12d5a9241af7fa17b9bdb7a5
Nicolas Otero <nicolasoterobouza@gmail.com>
2025-09-16 10:31:02 +0200

    Nuevo index

---
+++ StickerSmash/app/about.tsx
@@ -0,0 +1,25 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+const AboutScreen = () => (
+ 
+    <View style={styles.container}>
+    <Text style={styles.text}>About screen</Text>
+    </View>
+    );
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
+
+export default AboutScreen;
+
+
--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,8 +1,12 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+ import { Link } from 'expo-router'; 
 
-const Index=()=>(
+const Index = () => (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 
@@ -17,4 +21,11 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
   },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
 });
+
+export default Index;
